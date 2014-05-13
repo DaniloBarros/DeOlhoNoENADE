a3eeefe5ea06212fd60ecf6df35fb84bc0dd39cd
Guilherme da Luz <guilherme.daluz@outlook.com>
2014-05-13 13:41:09 -0300

    Criado Classe ControllerInstituicao

--- src/br/unb/deolhonoenade/controller/ControllerInstituicao.java
+++ src/br/unb/deolhonoenade/controller/ControllerInstituicao.java
@@ -1,9 +1,39 @@
 package br.unb.deolhonoenade.controller;
-import java.util.ArrayList;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import br.unb.deolhonoenade.DAO.ImportarBancoDeDados;
+import br.unb.deolhonoenade.DAO.OperacoesBancoDeDados;
 import br.unb.deolhonoenade.model.*;
 
 public class ControllerInstituicao {
 	
+private Instituicao instituicoes;
+	
+	private SQLiteDatabase db;
+	private OperacoesBancoDeDados opBD;
 
+	public ControllerInstituicao(Context context) {
+		ImportarBancoDeDados bdados = new ImportarBancoDeDados(context);
+		
+		SQLiteDatabase database = bdados.openDataBase();
+		
+		this.opBD = new OperacoesBancoDeDados(database);
+		
+		this.db=database;
+	}
+	
+	public Instituicao buscaInstituicao(int codIES){
+		
+		this.instituicoes = this.opBD.getIES(codIES);
+		
+		return instituicoes;
+	}
+/*
+	private SQLiteDatabase openDatabase(Context context) {
+		ImportarBancoDeDados db = new ImportarBancoDeDados(context);
+        return db.openDataBase();
+	}
+*/
 	
 }

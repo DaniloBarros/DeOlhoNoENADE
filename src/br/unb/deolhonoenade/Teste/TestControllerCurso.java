f7973cfd24e82dde678ee3a6d3fbc0e0f5194866
Guilherme da Luz <guilherme.daluz@outlook.com>
2014-05-30 02:08:18 -0300

    Signed-off-by BrunoCarbonell + Testes Controller

--- src/br/unb/deolhonoenade/Teste/TestControllerCurso.java
+++ src/br/unb/deolhonoenade/Teste/TestControllerCurso.java
@@ -1,11 +1,14 @@
 package br.unb.deolhonoenade.Teste;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import br.unb.deolhonoenade.DAO.OperacoesBancoDeDados;
 import br.unb.deolhonoenade.controller.ControllerCurso;
 import br.unb.deolhonoenade.model.Curso;
 import br.unb.deolhonoenade.model.Instituicao;
 import junit.framework.Assert;
+import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
 
 public class TestControllerCurso extends AndroidTestCase {
@@ -70,19 +73,145 @@ public class TestControllerCurso extends AndroidTestCase {
 	}
 
 	public void testBuscaCidades() {
-		fail("Not yet implemented");
+		ControllerCurso controller = new ControllerCurso(getContext());
+		List<String> cidadesT = new ArrayList<String>();
+		List<String> cidades = new ArrayList<String>();
+		
+		String cidade1 = new String("Todas");
+		String cidade2 = new String("CRUZEIRO DO SUL");
+		String cidade3 = new String("RIO BRANCO");
+		
+		cidadesT.add(cidade1);
+		cidadesT.add(cidade2);
+		cidadesT.add(cidade3);
+		
+		cidades = controller.buscaCidades(1, "AC");
+		Assert.assertEquals(cidadesT, cidades);
 	}
 
-	public void testBuscaTipos() {
-		fail("Not yet implemented");
+	// Busca tipos quando os tipos é só um tipo de instituição
+	public void testBuscaTiposAC() {
+		ControllerCurso controller = new ControllerCurso(getContext());
+		List<String> tiposT = new ArrayList<String>();
+		List<String> tipos = new ArrayList<String>();
+		
+		String tipo1 = new String("PRIVADA");
+
+		tiposT.add(tipo1);
+		
+		tipos = controller.buscaTipos(1, "RIO BRANCO");
+		Assert.assertEquals(tiposT, tipos);
+	}
+	
+	/* Busca tipos quando os tipos é mais de um tipo de instituição
+	 * Tipos devem ser Ambas, PRIVADA e PUBLICA
+	 */
+	public void testBuscaTiposDF() {
+		ControllerCurso controller = new ControllerCurso(getContext());
+		List<String> tiposT = new ArrayList<String>();
+		List<String> tipos = new ArrayList<String>();
+		
+		String tipo1 = new String("Ambas");
+		String tipo2 = new String("PRIVADA");
+		String tipo3 = new String("PUBLICA");
+
+		tiposT.add(tipo1);
+		tiposT.add(tipo2);
+		tiposT.add(tipo3);
+		
+		tipos = controller.buscaTipos(1, "BRASILIA");
+		Assert.assertEquals(tiposT, tipos);
 	}
 
+	/* Busca tipos quando os tipos é mais de um tipo de instituição
+	 * Tipos devem ser Ambas, PRIVADA e PUBLICA
+	 */
 	public void testBuscaTiposEstado() {
-		fail("Not yet implemented");
+		ControllerCurso controller = new ControllerCurso(getContext());
+		List<String> tiposT = new ArrayList<String>();
+		List<String> tipos = new ArrayList<String>();
+		
+		String tipo1 = new String("Ambas");
+		String tipo2 = new String("PRIVADA");
+		String tipo3 = new String("PUBLICA");
+
+		tiposT.add(tipo1);
+		tiposT.add(tipo2);
+		tiposT.add(tipo3);
+		
+		tipos = controller.buscaTiposEstado(1, "DF");
+		Assert.assertEquals(tiposT, tipos);
+	}
+	
+	// Busca tipos quando os tipos é só um tipo de instituição
+	public void testBuscaTiposEstadoAC() {
+		ControllerCurso controller = new ControllerCurso(getContext());
+		List<String> tiposT = new ArrayList<String>();
+		List<String> tipos = new ArrayList<String>();
+		
+		String tipo1 = new String("PRIVADA");
+
+		tiposT.add(tipo1);
+		
+		tipos = controller.buscaTiposEstado(1, "AC");
+		Assert.assertEquals(tiposT, tipos);
 	}
 
 	public void testBuscaUf() {
-		fail("Not yet implemented");
+		ControllerCurso controller = new ControllerCurso(getContext());
+		List<String> ufsT = new ArrayList<String>();
+		List<String> ufs = new ArrayList<String>();
+		
+		String uf1 = new String("AL");
+		String uf2 = new String("AM");
+		String uf3 = new String("AP");
+		String uf4 = new String("BA");
+		String uf5 = new String("CE");
+		String uf6 = new String("DF");
+		String uf7 = new String("GO");
+		String uf8 = new String("MA");
+		String uf9 = new String("MG");
+		String uf10 = new String("MS");
+		String uf11 = new String("MT");
+		String uf12 = new String("PA");
+		String uf13 = new String("PB");
+		String uf14 = new String("PE");
+		String uf15 = new String("PI");
+		String uf16 = new String("PR");
+		String uf17 = new String("RJ");
+		String uf18 = new String("RN");
+		String uf19 = new String("RR");
+		String uf20 = new String("RS");
+		String uf21 = new String("SC");
+		String uf22 = new String("SE");
+		String uf23 = new String("SP");
+
+		ufsT.add(uf1);
+		ufsT.add(uf2);
+		ufsT.add(uf3);
+		ufsT.add(uf4);
+		ufsT.add(uf5);
+		ufsT.add(uf6);
+		ufsT.add(uf7);
+		ufsT.add(uf8);
+		ufsT.add(uf9);
+		ufsT.add(uf10);
+		ufsT.add(uf11);
+		ufsT.add(uf12);
+		ufsT.add(uf13);
+		ufsT.add(uf14);
+		ufsT.add(uf15);
+		ufsT.add(uf16);
+		ufsT.add(uf17);
+		ufsT.add(uf18);
+		ufsT.add(uf19);
+		ufsT.add(uf20);
+		ufsT.add(uf21);
+		ufsT.add(uf22);
+		ufsT.add(uf23);
+		
+		ufs = controller.buscaUf(67);
+		Assert.assertEquals(ufsT, ufs);
 	}
 
 	public void testBuscaStringCursoIntString() {
